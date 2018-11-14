3a8b16c4253abdebebd0def9a6cd84fe63bf6c91
Saif Lakhani <saif@Amber.local>
2018-11-14 22:27:15 +0530

    Handle permissions

--- app/src/main/java/com/droidbots/areatravelogue/Splash.java
+++ app/src/main/java/com/droidbots/areatravelogue/Splash.java
@@ -1,9 +1,13 @@
 package com.droidbots.areatravelogue;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.v4.content.ContextCompat;
+import android.widget.Toast;
 
 
 public class Splash extends Activity {
@@ -15,7 +19,11 @@ public class Splash extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_spash);
 
-            requestPermissions(permissions,permsRequestCode);
+        requestPermissions(permissions,permsRequestCode);
+
+        if (ContextCompat.checkSelfPermission(Splash.this, Manifest.permission.CAMERA)
+                == PackageManager.PERMISSION_GRANTED) {
+            // Permission is granted
             new Handler().postDelayed(new Runnable() {
 
                 /*
@@ -36,5 +44,53 @@ public class Splash extends Activity {
             }, 2000);
 
 
+
+        }
+
+
+
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 200: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                    new Handler().postDelayed(new Runnable() {
+
+                        /*
+                         * Showing splash screen with a timer. This will be useful when you
+                         * want to show case your app logo / company
+                         */
+
+                        @Override
+                        public void run() {
+                            // This method will be executed once the timer is over
+                            // Start your app main activity
+                            Intent i = new Intent(Splash.this, MainActivity.class);
+                            startActivity(i);
+                            // close this activity
+                            finish();
+                        }
+                    }, 2000);
+
+                } else {
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                    Toast.makeText(Splash.this,"Camera permission not granted. Quitting",Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request.
+        }
     }
 }
