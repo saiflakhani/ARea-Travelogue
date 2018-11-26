b77e5333fbbffd142f2611caa9340fcbe64a451b
5abarinath <sabarinath1996@gmail.com>
2018-11-26 21:57:31 +0530

    Add POJO for Guide

---
+++ app/src/main/java/com/droidbots/areatravelogue/deals/Guide.java
@@ -0,0 +1,67 @@
+package com.droidbots.areatravelogue.deals;
+
+public class Guide {
+    private String name;
+    private int contact, id;
+    private String lat, lng, imgUrl;
+
+    public Guide() {
+    }
+
+    public Guide(int id, String name, String imgUrl, int contact, String lat, String lng) {
+        this.name = name;
+        this.contact = contact;
+        this.id = id;
+        this.lat = lat;
+        this.lng = lng;
+        this.imgUrl = imgUrl;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getContact() {
+        return contact;
+    }
+
+    public void setContact(int contact) {
+        this.contact = contact;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLat() {
+        return lat;
+    }
+
+    public void setLat(String lat) {
+        this.lat = lat;
+    }
+
+    public String getLng() {
+        return lng;
+    }
+
+    public void setLng(String lng) {
+        this.lng = lng;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+}
